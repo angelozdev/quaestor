318beef87270e1f22aee8e916041d063569b31bb
Angelo Zambrano <angelozam17@gmail.com>
2026-06-21 14:40:51 -0500

    feat(frontend/api): add barrel index

---
+++ frontend/lib/api/index.ts
@@ -0,0 +1,16 @@
+export * from "./auth";
+export * from "./transactions";
+export * from "./planned";
+export * from "./recurring";
+export * from "./accounts";
+export * from "./categories";
+export * from "./category-groups";
+export * from "./tags";
+export * from "./settings";
+export * from "./fx";
+export * from "./goals";
+export * from "./budgets";
+export * from "./reports";
+
+export { ApiError, setUnauthorizedHandler, onUnauthorized } from "./types";
+export type * from "./types";
